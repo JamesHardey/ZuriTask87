ff2adbcc09b9eae6edfc863688dd609db20a671c
JamesHardey <jamesade646@gmail.com>
2022-06-24 11:38:28 -0400

    initial commit

---
+++ aboutme.js
@@ -0,0 +1,7 @@
+var myName = "James Adefehinti";
+var height = "1.80"
+var country = "Nigeria";
+
+let output = "My name is "+myName + " and am "+height+" meter tall, from "+country;
+alert(output);
+console.log(output)
